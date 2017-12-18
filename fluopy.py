9596c9fb96320f31507b05e07d05aada6ea7c0e7
fluolabs <34490613+fluolabs@users.noreply.github.com>
2017-12-18 11:32:46 -0600

    Update fluopy.py

--- fluopy.py
+++ fluopy.py
@@ -1,4 +1,4 @@
-def showGrid(s=20, black=155, text=True):
+def showGrid(s=20, black=155, num=True):
     """Display grid lines"""
     rows = height/s
     cols = width/s
@@ -8,6 +8,12 @@ def showGrid(s=20, black=155, text=True):
     for i in range(cols):
         line(s*i, 0, s*i, height)
         
-    if (text):
+    if (num):
+        text("   0", 0, 10)
         text(str(width), width - 20, 10)
+        #pushStyle()
+        fill(255, 0, 0)
+        text("0", 0, 10)
         text(str(height), 0, height)
+        fill(0)
+        #popStyle()
